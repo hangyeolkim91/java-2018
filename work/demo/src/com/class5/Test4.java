ad2d5b3751d98a0657c157d906101047d46245d3
hangyeolkim91 <hangyeoulkim@gmail.com>
2018-05-31 14:09:34 +0900

    for change folder

--- work/demo/src/com/class5/Test4.java
+++ work/demo/src/com/class5/Test4.java
@@ -41,12 +41,17 @@ class SubClass extends SuperClass{
 public class Test4 {
 
 	public static void main(String[] args) {
-
+		
 		SubClass ob = new SubClass();
 		
-		SuperClass sc = new SubClass();
+		SuperClass superClass = new SuperClass();
 		
-		ob.write();
+		SuperClass sc = ob;
+		
+		sc.write();
+		
+		
+		/*ob.write();
 		
 		ob.print1();
 		
@@ -57,16 +62,23 @@ public class Test4 {
 		System.out.println("ob.b : " + ob.b);
 		
 		System.out.println("( (SuperClass)ob).b : " + ( (SuperClass)ob).b); //UPCAST
-		
+*/		
 		
 		
 		// 메소드는 인스턴스변수와 다르게 자신의 메소드를 호출
 		// 메소드는 heap영역이 아닌 메소드 영역에 저장되어있기 때문에
 		// 접근이 불가 하다
 		// 메소드는 UPCAST해도 무조건 자신것을 사용한다.
-		((SuperClass)ob).write();
+		/*((SuperClass)ob).write();
 		
-		sc.write();
+		((SubClass)superClass).write();*/
+		
+		//sc.write();
+		
+		//sc.print1(); 안됨
+		
+		/*SubClass child = (SubClass) sc;
+		child.print1();*/
 	}
 
 }
---
+++ work/demo/src/com/class6/Test1.java
@@ -0,0 +1,31 @@
+package com.class6;
+
+class TestA{
+	
+	public void print(){
+		System.out.println("A클래스...");
+	}
+	
+}
+
+class TestB{
+	
+	public void print(){
+		System.out.println("B클래스...");
+	}
+	
+}
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		TestA a = new TestA();
+		TestB b = new TestB();
+		
+		/*
+		 * 부모와 자식관계(상속관계)
+		 */
+	}
+
+}
